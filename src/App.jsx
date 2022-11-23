b57efa432f571b09d15700f5019d6620e11c22ea
radisavljevicmarko <markoza.care1402@gmail.com>
2022-11-23 19:37:07 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./App.css";
+//imooirting components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Marko's Todo List</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
